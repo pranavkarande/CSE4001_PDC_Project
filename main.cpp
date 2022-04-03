8c7df577b28cc0f056c629fb29eba2663f9515f4
Pranav Karande <pranav.3943@gmail.com>
2022-04-03 23:08:27 +0530

    Implemented Serial BFS
    Used pointers to represent nodes in the graph and implemented and primitively tested serial BFS

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ using namespace std;
 // Class to encapsulate node
 class node {
 public:
+  // Each node has an integer ID corrosponding
+  // to it's index in the edgeMatrix
+  short unsigned int id;
+
+  // Static counter to assign default value to id
+  inline static short unsigned int counter;
+
   // Has node been discoverd by BFS
   bool discovered;
 
@@ -17,6 +24,7 @@ public:
   // Construct undiscovered node with
   // infinite(-1) distance form root
   node() {
+    id = counter++;
     discovered = false;
     d = -1;
   }
@@ -43,6 +51,7 @@ public:
         vector<vector<unsigned short int>>(n, vector<unsigned short int>(n));
 
     // Initialize node vector
+    node::counter = 0;
     N = vector<node>(n);
   }
 
@@ -63,34 +72,57 @@ public:
     }
   }
 
+  // Print distance of all nodes after BFS
+  void print_distance() {
+    cout << "Node ID\t\tDistance" << endl;
+    cout << "-------------------" << endl;
+    for (auto i : N) {
+      cout << i.id << "\t\t" << i.d << endl;;
+    }
+    cout << "-------------------" << endl;
+  }
+
   // Serial BFS
-  void BFS_serial(node &);
+  void BFS_serial(unsigned short int);
 };
 
 // TODO Implement serial BFS
-void graph::BFS_serial(node &s) {
+void graph::BFS_serial(unsigned short int s_id) {
+  node *s = &N[s_id];
   // BFS Queue
-  queue<node> Q;
+  queue<node *> Q;
+
+
 
   // Distance of source will be 0
-  s.d = 0;
+  s->d = 0;
   // Source has been discovered
-  s.discovered = true;
+  s->discovered = true;
 
   Q.push(s);
 
   while (!Q.empty()) {
+    node *u = Q.front();
+    Q.pop();
+    for (int v = 0; v < no_of_nodes; ++v) {
+      if ((edgeMatrix[u->id][v] != 0) && (!N[v].discovered)) {
+        N[v].discovered = true;
+        N[v].d = u->d + edgeMatrix[u->id][v];
+        Q.push(&N[v]);
+      }
+    }
+    u->discovered = true;
   }
 }
 
 int main(void) {
-  graph G(10);
+  graph G(4);
 
+  G.add_edge(0, 1, 1);
   G.add_edge(0, 3, 2);
-  G.add_edge(4, 9, 8);
-  G.add_edge(2, 4, 4);
-  G.add_edge(5, 2, 2);
-  G.add_edge(2, 6, 1);
+  G.add_edge(1, 2, 3);
+  G.add_edge(3, 4, 4);
 
-  G.print_matrix();
+  G.BFS_serial(0);
+  G.print_distance();
 }
