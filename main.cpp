0e02ef89b2e4c58497eb700cd833ca2bc053d4df
Pranav Karande <pranav.3943@gmail.com>
2022-04-04 00:05:51 +0530

    Tested serial BFS
    Graph is now unweighted and serial BSF has been tested

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ public:
   unsigned short int no_of_nodes;
 
   // Edge matrix vector
-  vector<vector<unsigned short int>> edgeMatrix;
+  vector<vector<bool>> edgeMatrix;
 
   // Node vector
   vector<node> N;
@@ -48,20 +48,19 @@ public:
 
     // Initialize edgeMatrix
     edgeMatrix =
-        vector<vector<unsigned short int>>(n, vector<unsigned short int>(n));
+        vector<vector<bool>>(n, vector<bool>(n));
 
     // Initialize node vector
     node::counter = 0;
     N = vector<node>(n);
   }
 
-  // Add edge from node s to node d with weight w
-  void add_edge(unsigned short int s, unsigned short int d,
-                unsigned short int w) {
-    edgeMatrix[s][d] = w;
+  // Add edge from node s to node d
+  void add_edge(unsigned short int s, unsigned short int d) {
+    edgeMatrix[s][d] = true;
   }
 
-  // Print graph matrix
+  // Print adjacency matrix
   void print_matrix() {
     cout << "Adjacency Matrix of the graph is: " << endl;
     for (auto i : edgeMatrix) {
@@ -86,13 +85,11 @@ public:
   void BFS_serial(unsigned short int);
 };
 
-// TODO Implement serial BFS
 void graph::BFS_serial(unsigned short int s_id) {
   node *s = &N[s_id];
+
   // BFS Queue
   queue<node *> Q;
-
-
 
   // Distance of source will be 0
   s->d = 0;
@@ -105,9 +102,9 @@ void graph::BFS_serial(unsigned short int s_id) {
     node *u = Q.front();
     Q.pop();
     for (int v = 0; v < no_of_nodes; ++v) {
-      if ((edgeMatrix[u->id][v] != 0) && (!N[v].discovered)) {
+      if ((edgeMatrix[u->id][v] != false) && (!N[v].discovered)) {
         N[v].discovered = true;
-        N[v].d = u->d + edgeMatrix[u->id][v];
+        N[v].d = u->d + 1;
         Q.push(&N[v]);
       }
     }
@@ -116,12 +113,37 @@ void graph::BFS_serial(unsigned short int s_id) {
 }
 
 int main(void) {
-  graph G(4);
+  graph G(18);
 
-  G.add_edge(0, 1, 1);
-  G.add_edge(0, 3, 2);
-  G.add_edge(1, 2, 3);
-  G.add_edge(3, 4, 4);
+  G.add_edge(0, 4);
+  G.add_edge(4, 1);
+  G.add_edge(0, 1);
+  G.add_edge(5, 4);
+  G.add_edge(2, 5);
+  G.add_edge(5, 6);
+  G.add_edge(4, 7);
+  G.add_edge(4, 8);
+  G.add_edge(8, 5);
+  G.add_edge(9, 5);
+  G.add_edge(9, 6);
+  G.add_edge(6, 10);
+  G.add_edge(7, 8);
+  G.add_edge(8, 9);
+  G.add_edge(7, 14);
+  G.add_edge(11, 7);
+  G.add_edge(9, 13);
+  G.add_edge(10, 17);
+  G.add_edge(11, 12);
+  G.add_edge(12, 13);
+  G.add_edge(13, 16);
+  G.add_edge(13, 17);
+  G.add_edge(14, 15);
+  G.add_edge(6, 5);
+  G.add_edge(8, 7);
+  G.add_edge(12, 11);
+  G.add_edge(13, 12);
+  G.add_edge(16, 13);
+  G.add_edge(15, 14);
 
   G.BFS_serial(0);
   G.print_distance();
