3e16f1110c5c3c9a21c664841c07117ac9e2b2f2
Pranav Karande <pranav.3943@gmail.com>
2022-04-05 00:20:23 +0530

    Implemented File Handling
    Main now reads data from files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include <fstream>
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <omp.h>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #define NO_OF_THREADS 4
 
@@ -148,6 +151,7 @@ void Graph::BFS_parallel(unsigned short int s_id) {
       if ((edgeMatrix[u->id][v]) && (!N[v].discovered_parallel)) {
         N[v].discovered_parallel = true;
         N[v].d_parallel = u->d_parallel + 1;
+
         Q.push(&N[v]);
       }
     }
@@ -157,37 +161,85 @@ void Graph::BFS_parallel(unsigned short int s_id) {
 
 int main(void) {
   omp_set_num_threads(NO_OF_THREADS);
-  Graph G(18);
 
-  G.add_edge(0, 4);
-  G.add_edge(4, 1);
-  G.add_edge(0, 1);
-  G.add_edge(5, 4);
-  G.add_edge(2, 5);
-  G.add_edge(5, 6);
-  G.add_edge(4, 7);
-  G.add_edge(4, 8);
-  G.add_edge(8, 5);
-  G.add_edge(9, 5);
-  G.add_edge(9, 6);
-  G.add_edge(6, 10);
-  G.add_edge(7, 8);
-  G.add_edge(8, 9);
-  G.add_edge(7, 14);
-  G.add_edge(11, 7);
-  G.add_edge(9, 13);
-  G.add_edge(10, 17);
-  G.add_edge(11, 12);
-  G.add_edge(12, 13);
-  G.add_edge(13, 16);
-  G.add_edge(13, 17);
-  G.add_edge(14, 15);
-  G.add_edge(6, 5);
-  G.add_edge(8, 7);
-  G.add_edge(12, 11);
-  G.add_edge(13, 12);
-  G.add_edge(16, 13);
-  G.add_edge(15, 14);
+  // Graph G(18);
+
+  // G.add_edge(0, 4);
+  // G.add_edge(4, 1);
+  // G.add_edge(0, 1);
+  // G.add_edge(5, 4);
+  // G.add_edge(2, 5);
+  // G.add_edge(5, 6);
+  // G.add_edge(4, 7);
+  // G.add_edge(4, 8);
+  // G.add_edge(8, 5);
+  // G.add_edge(9, 5);
+  // G.add_edge(9, 6);
+  // G.add_edge(6, 10);
+  // G.add_edge(7, 8);
+  // G.add_edge(8, 9);
+  // G.add_edge(7, 14);
+  // G.add_edge(11, 7);
+  // G.add_edge(9, 13);
+  // G.add_edge(10, 17);
+  // G.add_edge(11, 12);
+  // G.add_edge(12, 13);
+  // G.add_edge(13, 16);
+  // G.add_edge(13, 17);
+  // G.add_edge(14, 15);
+  // G.add_edge(6, 5);
+  // G.add_edge(8, 7);
+  // G.add_edge(12, 11);
+  // G.add_edge(13, 12);
+  // G.add_edge(16, 13);
+  // G.add_edge(15, 14);
+
+  ifstream fin;
+  string line;
+  int n = 0;
+
+  // by default open mode = ios::in mode
+  fin.open("test.txt");
+
+  // Execute a loop until EOF (End of File)
+  while (fin) {
+    // Read a Line from File
+    getline(fin, line);
+
+    // Print line in Console
+    if (line[0] != '#') {
+      n = stoi(line);
+      cout << line << endl;
+      break;
+    }
+  }
+
+  Graph G(n);
+
+  while (fin) {
+
+    // Read a Line from File
+    getline(fin, line);
+
+    istringstream ss(line);
+
+    // For storing each word
+    string word;
+
+    // Traverse through all words
+    // while loop till we get
+    // strings to store in string word
+    while (ss >> word) {
+      unsigned short int n1 = stoi(word);
+      ss >> word;
+      unsigned short int n2 = stoi(word);
+
+      G.add_edge(n1, n2);
+    }
+  }
+
+  // Close the file
+  fin.close();
 
   G.BFS_serial(0);
   G.BFS_parallel(0);
