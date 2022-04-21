e63f439d136a059478312042678a81469279f394
Rohit Kumar Rout <79717560+Rohitrout416@users.noreply.github.com>
2022-04-22 01:03:03 +0530

    added automation script to do the boring stuff
    runs the exec file on each file with different number of threads ranging from 1 to the maximum number of threads in the computer

--- script.py
+++ script.py
@@ -1,6 +1,39 @@
-from subprocess import Popen
+import os
+import subprocess
+import sys
+import platform
 
-for i in range(1, 17):
-    #put the path of the executable
-    p = Popen(['/Users/rohitkumarrout/Desktop/new/1',str(i)])
+output = []
+single_thread_sum = 0.0
+no_of_threads = os.cpu_count()
+files = ['p2p-Gnutella08.txt', 'test.txt', 'soc-Epinions1.txt']
+
+f = open('output.txt', 'w')
+
+for file_name in files:
+    f.write("Time taken for "+file_name+':'+'\n')
+    for _ in range(3):
+        print('Running on 1 thread...', end='\r')
+        p = subprocess.run(['/workspace/CSE4001_PDC_Project/main', '1',
+                            file_name], capture_output=True)
+        x = p.stdout.decode("utf-8")
+        splitting = x.split('h')
+        single_thread_sum += float(splitting[1])/3
+    output.append(single_thread_sum)
+
+    for i in range(2, no_of_threads+1):
+        print('Running on', str(i), 'threads...', end='\r')
+        p = subprocess.run(['/workspace/CSE4001_PDC_Project/main', str(i),
+                            file_name], capture_output=True)
+        x = p.stdout.decode("utf-8")
+        splitting = x.split('h')
+        output.append(float(splitting[1]))
+
+    for i in range(16):
+        element = output[i]
+        element = f'{element:.3f}'
+        f.write(str(i+1)+'. '+str(element)+'\n')
     
+    f.write('\n')
+
+f.close()
